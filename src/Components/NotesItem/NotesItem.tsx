bc4b54305863ff05958d0f868d11490a4f37ebb7
HariGadi123 <hari@g10consultancy.com>
2026-06-05 19:57:50 +0530

    worked on the notes app

--- src/Components/NotesItem/NotesItem.tsx
+++ src/Components/NotesItem/NotesItem.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const NotesItem: React.FC = () => {
-  return <div>NotesItem</div>;
+interface NoteItemProps {
+  item: {
+    title: string;
+    description: string;
+  };
+}
+const NotesItem: React.FC<NoteItemProps> = ({ item }) => {
+  return (
+    <div className="flex flex-col gap-1 justify-center p-3 box-border border-2 border-gray-300 rounded-2xl">
+      <h3 className="font-semibold">{item?.title}</h3>
+      <p>{item?.description}</p>
+    </div>
+  );
 };
 
 export default NotesItem;
--- src/Components/NotesListing/NotesListing.tsx
+++ src/Components/NotesListing/NotesListing.tsx
@@ -1,5 +1,6 @@
 import { Button, TextareaAutosize, TextField } from "@mui/material";
 import React, { useState } from "react";
+import NotesItem from "../NotesItem/NotesItem";
 
 interface FormData {
   title: string;
@@ -13,6 +14,10 @@ const NotesListing: React.FC = () => {
 
   const [notesData, setNotesData] = useState<FormData[]>([]);
   const handleAddItem = () => {
+    if (formData.title === "" || formData.description === "") {
+      alert("Please fill in all fields");
+      return;
+    }
     setNotesData([...notesData, formData]);
     setFormData({
       title: "",
@@ -21,7 +26,7 @@ const NotesListing: React.FC = () => {
   };
   return (
     <div className="flex flex-col justify-center items-center p-10 shadow-lg rounded-xl box-content w-[60%] gap-5">
-      <h1>Notes</h1>
+      <h1 className="text-[#4c63b6] font-semibold text-5xl"> Notes</h1>
       <div className="flex flex-col justify-center items-center gap-3 p-4 box-content rounded-xl w-[70%] border-2 border-gray-300">
         <TextField
           label="Title"
@@ -37,7 +42,7 @@ const NotesListing: React.FC = () => {
           onChange={(e) =>
             setFormData({ ...formData, description: e.target.value })
           }
-          className="w-full"
+          className="w-full p-4 box-border rounded-lg border-2 border-gray-300"
           maxRows={10}
           minRows={10}
         />
@@ -49,6 +54,25 @@ const NotesListing: React.FC = () => {
           Add
         </Button>
       </div>
+      {notesData?.length === 0 ? (
+        <div className="flex flex-col justify-center items-center gap-3">
+          <img
+            src="https://assets.ccbp.in/frontend/hooks/empty-notes-img.png"
+            alt="notes_empty"
+            className="w-50 h-50"
+          />
+          <h1 className="text-[#475569] font-semibold text-xl">No Notes Yet</h1>
+          <p className="text-[#475569] font-light text-lg">
+            Notes yo added will appear here.
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-row w-full justify-start gap-3 flex-wrap mt-5" >
+          {notesData.map((note) => (
+            <NotesItem item={note} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
